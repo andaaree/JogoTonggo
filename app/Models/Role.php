5deead120ae09b6ea0a510fe94886b7740bda281
Andar <50786504+andaaree@users.noreply.github.com>
2023-09-08 22:52:45 +0700

    SoftDeletes
    add Region, Role, User softdeletes

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     public function users()
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use App\Models\Role;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
